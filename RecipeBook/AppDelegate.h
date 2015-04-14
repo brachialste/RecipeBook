dba6b7da4eb07cb5f1bf911809a5b28acddfa56c
Pegasus <cortespc@macbook-pro-de-jorge-cortes.local>
2015-04-14 13:28:15 -0500

    Initial Commit

---
+++ RecipeBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecipeBook
+//
+//  Created by Pegasus on 14/04/15.
+//  Copyright (c) 2015 Pegasus Control. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecipeBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecipeBook
+//
+//  Created by Pegasus on 14/04/15.
+//  Copyright (c) 2015 Pegasus Control. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
